f77534ca4c390ddce4b30d1029e8b7ab8ccc1159
dexter <dexter@Dexter>
2016-03-05 09:32:33 -0500

    --Editado 01 Dexter --

--- Proyect/src/Reportes/NewClass.java
+++ Proyect/src/Reportes/NewClass.java
@@ -3,19 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyect;
+package Reportes;
+
+import DAO.*;
 
 /**
  *
  * @author dexter
  */
-public class Proyect {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class NewClass {
     
 }
